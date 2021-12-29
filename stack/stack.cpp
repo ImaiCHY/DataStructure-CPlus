cad1b903da122d03d5bbf04d411b70e79ff5024e
ImaiLisa <imailisa@imailisa.com>
2021-12-29 23:32:41 +0800

    Day0

---
+++ stack/stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 12/29/21.
+//
+
+#include "stack.h"
---
+++ stack/stack.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 12/29/21.
+//
+
+#ifndef DATASTRUCTURE_STACK_H
+#define DATASTRUCTURE_STACK_H
+
+
+class stack {
+
+};
+
+
+#endif //DATASTRUCTURE_STACK_H
